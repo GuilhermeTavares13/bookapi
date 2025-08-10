a9e926b64085af865f62441a539127ab7439dad4
Guilherme Tavares <gui.tavares2002.contato@gmail.com>
2025-08-10 12:05:47 -0300

    Add files

---
+++ BookApi/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyCors", builder =>
+    {
+        builder
+        .WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors("MyCors");
+
+app.MapControllers();
+
+app.MapGet("/", () => {
+    Results.Redirect("/api/books");
+});
+
+app.Run();
